21805d5d5dee787b7319b8f94a4b5396b1599e32
kjvalencik <kjvalencik@gmail.com>
2020-08-01 18:11:03 +0000

    Deploying to gh-pages from  @ f70f4b825be7444a900bfd2ea7f033e42ed786fe 🚀

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,10 +1,3 @@
 (function() {var implementors = {};
-implementors["neon_runtime"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"neon_runtime/call/struct.CCallback.html\" title=\"struct neon_runtime::call::CCallback\">CCallback</a>",synthetic:false,types:["neon_runtime::call::CCallback"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        
-})()
+implementors["neon_sys"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"neon_sys/struct.CCallback.html\" title=\"struct neon_sys::CCallback\">CCallback</a>","synthetic":false,"types":["neon_sys::CCallback"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,9 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["cfg_if"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["cslice"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["neon"] = {"name":"","dirs":[{"name":"borrow","files":["internal.rs","mod.rs"]},{"name":"context","files":["internal.rs","mod.rs"]},{"name":"handle","files":["internal.rs","mod.rs"]},{"name":"macro_internal","files":["mod.rs"]},{"name":"object","dirs":[{"name":"class","files":["internal.rs","mod.rs"]}],"files":["mod.rs"]},{"name":"result","files":["mod.rs"]},{"name":"task","files":["mod.rs"]},{"name":"types","files":["binary.rs","error.rs","internal.rs","mod.rs","utf8.rs"]}],"files":["lib.rs","meta.rs","prelude.rs"]};
+sourcesIndex["neon_runtime"] = {"name":"","dirs":[{"name":"nan","files":["array.rs","arraybuffer.rs","buffer.rs","call.rs","class.rs","convert.rs","error.rs","fun.rs","handler.rs","mem.rs","mod.rs","module.rs","object.rs","primitive.rs","raw.rs","scope.rs","string.rs","tag.rs","task.rs","try_catch.rs"]}],"files":["lib.rs"]};
+sourcesIndex["neon_sys"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["semver"] = {"name":"","files":["lib.rs","version.rs","version_req.rs"]};
+sourcesIndex["semver_parser"] = {"name":"","files":["common.rs","lib.rs","range.rs","recognize.rs","version.rs"]};
+createSourceSidebar();
